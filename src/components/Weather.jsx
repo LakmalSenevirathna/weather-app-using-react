50fcd8244191b5ec08a5f8ac3bf721ae3a912372
Lakmal Senevirathna <laka.usjp@gmail.com>
2024-07-07 20:30:49 +0300

    Add API call to fetch weather data

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import './Weather.css'
 import search_icon from '../assets/search.png'
 import clear_icon from '../assets/clear.png'
@@ -10,31 +10,100 @@ import snow_icon from '../assets/snow.png'
 import wind_icon from '../assets/wind.png'
 
 const Weather = () => {
+    // Create a ref to store the input element, allowing access to its value
+    const inputRef = useRef();
+
+    const [weatherData,setWeatherData] = useState(false);
+
+    // Mapping weather codes to corresponding icons
+    const allIcons = {
+        "01d": clear_icon,
+        "01n": clear_icon,
+        "02d": cloud_icon,
+        "02n": cloud_icon,
+        "03d": cloud_icon,
+        "03n": cloud_icon,
+        "04d": drizzle_icon,
+        "04n": drizzle_icon,
+        "09d": rain_icon,
+        "09n": rain_icon,
+        "10d": rain_icon,
+        "10n": rain_icon,
+        "13d": snow_icon,
+        "13n": snow_icon
+    }
+
+    // Function to fetch and set weather data based on the city
+    const search = async (city) => {
+        if(city === ""){
+            alert("Enter city name");
+            return;
+        }
+        try{
+            let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${import.meta.env.VITE_APP_ID}`;
+
+            const response = await fetch(url);
+            const data = await response.json();
+
+            if(!response.ok){
+                alert(data.message);
+                return;
+            }
+
+            console.log(data);
+            // Get icon code
+            const icon = allIcons[data.weather[0].icon] || clear_icon;
+            // Set the weather data to state
+            setWeatherData({
+                humidity: data.main.humidity,
+                windSpeed: data.wind.speed,
+                temperature: Math.floor(data.main.temp),
+                location: data.name,
+                icon: icon
+
+            })
+
+        }catch(error){
+            setWeatherData(false);
+            console.log("Error in fetching weather data");
+        }
+    }
+
+    // Fetch the weather data for "Helsinki" on initial rendering
+    useEffect(()=>{
+        search("Helsinki");
+    },[])
+
   return (
     <div className='weather'>
       <div className="search-bar">
-        <input type="text" placeholder='search' />
-        <img src={search_icon} alt="" />
+        <input ref={inputRef} type="text" placeholder='search' />
+        <img src={search_icon} onClick={()=>search(inputRef.current.value)} alt="" />
       </div>
-      <img src={clear_icon} alt="" className='weather-icon' />
-      <p className="temperature">16°c</p>
-      <p className="location">London</p>
+      {weatherData?
+      <>
+      <img src={weatherData.icon} alt="" className='weather-icon' />
+      <p className="temperature">{weatherData.temperature}°c</p>
+      <p className="location">{weatherData.location}</p>
       <div className="weather-data">
         <div className="col">
             <img src={humidity_icon} alt="" />
             <div>
-                <p>91 %</p>
+                <p>{weatherData.humidity}%</p>
                 <span>Humidity</span>
             </div>
         </div>
         <div className="col">
             <img src={wind_icon} alt="" />
             <div>
-                <p>3.6 km/h</p>
+                <p>{weatherData.windSpeed}km/h</p>
                 <span>Wind Speed</span>
             </div>
         </div>
       </div>
+      </>:
+      <></>}
+
     </div>
   )
 }
